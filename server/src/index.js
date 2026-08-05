6133e6d9ec4f84d6582b3ce5e32f7ad2f55f52f8
Matthew Martinez <mmart570@ucr.edu>
2026-08-04 20:40:30 -0700

    connect express to postgres

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,13 @@
 const express = require('express');
+const pool = require('./db');
+
+pool.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.error('Database connection failed:', err);
+  } else {
+    console.log('Database connected at:', res.rows[0].now);
+  }
+});
 const app = express();
 
 app.use(express.json());
